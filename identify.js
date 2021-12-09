ebec3e0c5e2aba805c418d86ddfde28b0367824d
Adrien Burgun <adrien.burgun@orange.fr>
2021-12-09 09:49:43 +0100

    :bug: Fix identify token regeneration period

--- identify.js
+++ identify.js
@@ -8,13 +8,13 @@ let identify_rest = new Rest(IDENTIFY_HOSTNAME);
 let identify_cache;
 let identify_timeout = 0;
 function identify() {
-    if (new Date().getSeconds() >= identify_timeout) {
+    if (new Date().getTime() >= identify_timeout) {
         return new Promise(async (resolve, reject) => {
             let res = await identify_rest.post("/v1/accounts:signUp?key=" + secret.identify_key, {
                 key: secret.identify_key
             });
             identify_cache = res.idToken;
-            identify_timeout = new Date().getSeconds() + +res.expiresIn - 30;
+            identify_timeout = new Date().getTime() + 1000 * +res.expiresIn - 30000;
             resolve(identify_cache);
         });
     } else {
