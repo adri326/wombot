2af744d2213989d6b8e6c8b622d3ba698a7fdec2
leopoldhub <leopold12.hub@gmail.com>
2022-01-13 20:44:03 +0100

    ✨🎨 Code refactoring and synchronous multiple generations

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 const task = require("./index.js");
 const styles = require("./styles.js");
 const yargs = require("yargs");
-const { hideBin } = require('yargs/helpers');
+const { hideBin } = require("yargs/helpers");
 
 const argc = yargs(hideBin(process.argv))
     .scriptName("wombot")
@@ -38,6 +38,11 @@ const argc = yargs(hideBin(process.argv))
         default: false,
         describe: "When set, disables the download of the final image. Instead, its url is printed."
     })
+    .option("noasync", {
+        type: "boolean",
+        default: false,
+        describe: "When set, disables the asyncronous generation. May avoid rate limiting.",
+    })
     .alias("h", "help")
     .parse();
 
@@ -54,74 +59,54 @@ const quiet = argc.quiet;
 const inter = argc.inter;
 const final = !argc.nofinal;
 
+async function generate(prompt, style, prefix) {
+	function handler(data, prefix) {
+		switch (data.state) {
+			case "authenticated":
+				if (!quiet) console.log(`${prefix}Authenticated, allocating a task...`);
+				break;
+			case "allocated":
+				if (!quiet) console.log(`${prefix}Allocated, submitting the prompt and style...`);
+				break;
+			case "submitted":
+				if (!quiet) console.log(`${prefix}Submitted! Waiting on results...`);
+				break;
+			case "progress":
+				let current = data.task.photo_url_list.length;
+				let max = styles.steps.get(style) + 1;
+				if (!quiet) console.log(`${prefix}Submitted! Waiting on results... (${current}/${max})`);
+				break;
+			case "generated":
+				if (!quiet) console.log(`${prefix}Results are in, downloading the final image...`);
+				break;
+			case "downloaded":
+				if (!quiet) console.log(`${prefix}Downloaded!`);
+				break;
+		}
+	}
+
+	let res = await task(prompt, style, (data) => handler(data, prefix), {final, inter});
+	if (!quiet && final)
+		console.log(`${prefix}Your results have been downloaded to the following files:`);
+	else if (!quiet)
+		console.log(`${prefix}Task finished, the results are available at the following addresses:`);
+
+	for (let inter of res.inter)
+		console.log(inter);
+	
+	if (final) console.log(res.path);
+	else console.log(res.url);
+}
+
 (async () => {
-    let prompt = argc._[0];
-    let style = +argc._[1] || 3;
-    if (!quiet) console.log("Prompt: `" + prompt + "`, Style: `" + styles.get(style) + "`");
+	let prompt = argc._[0];
+	let style = +argc._[1] || 3;
+	if (!quiet)
+		console.log(`Prompt: \`${prompt}\`, Style: \`${styles.get(style)}\``);
 
-    if (argc.times > 1) { // --times > 1
-        let promises = [];
-        let states = [];
-
-        function handler(data, n) {
-            let current = data.task?.photo_url_list?.length ?? 0;
-            let max = styles.steps.get(style) + 1;
-            states[n] = data.state;
-
-            if (!quiet) console.log(n + ": " + data.state + " (" + current + "/" + max + ")");
-        }
-
-        for (let n = 0; n < +argc.times; n++) {
-            promises.push(task(prompt, style, (data) => handler(data, n), {final, inter}));
-
-            states.push("initializing");
-        }
-
-        let res = await Promise.all(promises);
-        if (!quiet && final) console.log("Your results have been downloaded to the following files:");
-        else if (!quiet) console.log("Task finished, the results are available at the following addresses:");
-
-        for (let r of res) {
-            for (let inter of r.inter) {
-                console.log(inter);
-            }
-            if (final) console.log(r.path);
-            else console.log(res.url);
-        }
-    } else { // --times == 1
-        function handler(data) {
-            switch (data.state) {
-                case "authenticated":
-                    if (!quiet) console.log("Authenticated, allocating a task...");
-                    break;
-                case "allocated":
-                    if (!quiet) console.log("Allocated, submitting the prompt and style...");
-                    break;
-                case "submitted":
-                    if (!quiet) console.log("Submitted! Waiting on results...");
-                    break;
-                case "progress":
-                    let current = data.task.photo_url_list.length;
-                    let max = styles.steps.get(style) + 1;
-                    if (!quiet) console.log("Submitted! Waiting on results... (" + current + "/" + max + ")");
-                    break;
-                case "generated":
-                    if (!quiet) console.log("Results are in, downloading the final image...");
-                    break;
-                case "downloaded":
-                    if (!quiet) console.log("Downloaded!");
-                    break;
-            }
-        }
-
-        let res = await task(prompt, style, handler, {final, inter});
-        if (!quiet && final) console.log("Your results have been downloaded to the following files:");
-        else if (!quiet) console.log("Task finished, the results are available at the following addresses:");
-
-        for (let inter of res.inter) {
-            console.log(inter);
-        }
-        if (final) console.log(res.path);
-        else console.log(res.url);
-    }
+	for (let n = 0; n < +argc.times; n++) {
+		const prefix = argc.times == 1 ? `` : `${n+1}: `;
+		if (argc.noasync) await generate(prompt, style, prefix);
+		else generate(prompt, style, prefix);
+	}
 })();
