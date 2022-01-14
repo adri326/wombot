c5849c9817e598e3e1f8183853e25364d82765dd
Adrien Burgun <adrien.burgun@orange.fr>
2022-01-14 22:44:55 +0100

    :sparkles: Add steps for styles "S. Dali" and "Wuhtercuhler"

--- styles.js
+++ styles.js
@@ -1,8 +1,8 @@
 let styles = new Map();
-styles.set(1, "Synthwave"); // 23 steps
+styles.set(1, "Synthwave");
 styles.set(2, "Ukiyoe");
 styles.set(3, "No Style");
-styles.set(4, "Steampunk"); // 24 steps
+styles.set(4, "Steampunk");
 styles.set(5, "Fantasy Art");
 styles.set(6, "Vibrant");
 styles.set(7, "HD");
@@ -13,10 +13,10 @@ styles.set(11, "Mystical");
 styles.set(12, "Festive");
 styles.set(13, "Baroque");
 styles.set(14, "Etching");
-styles.set(15, "S Dali");
+styles.set(15, "S. Dali"); // Note: The website didn't put a space in it
 styles.set(16, "Wuhtercuhler");
 
-
+// Counts the number of GANs in the chain, used for debugging
 // Does not include the "final" image!
 let steps = new Map();
 steps.set(1, 23);
@@ -33,6 +33,8 @@ steps.set(11, 20);
 steps.set(12, 32);
 steps.set(13, 20);
 steps.set(14, 20);
+steps.set(15, 20);
+steps.set(16, 20);
 
 module.exports = styles;
 module.exports.steps = steps;
