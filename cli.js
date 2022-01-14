b311bcd006472e08a937691143b0679088a14f70
Adrien Burgun <adrien.burgun@orange.fr>
2022-01-14 22:15:35 +0100

    :art: Convert tabs to spaces in cli.js

--- cli.js
+++ cli.js
@@ -60,53 +60,53 @@ const inter = argc.inter;
 const final = !argc.nofinal;
 
 async function generate(prompt, style, prefix) {
-	function handler(data, prefix) {
-		switch (data.state) {
-			case "authenticated":
-				if (!quiet) console.log(`${prefix}Authenticated, allocating a task...`);
-				break;
-			case "allocated":
-				if (!quiet) console.log(`${prefix}Allocated, submitting the prompt and style...`);
-				break;
-			case "submitted":
-				if (!quiet) console.log(`${prefix}Submitted! Waiting on results...`);
-				break;
-			case "progress":
-				let current = data.task.photo_url_list.length;
-				let max = styles.steps.get(style) + 1;
-				if (!quiet) console.log(`${prefix}Submitted! Waiting on results... (${current}/${max})`);
-				break;
-			case "generated":
-				if (!quiet) console.log(`${prefix}Results are in, downloading the final image...`);
-				break;
-			case "downloaded":
-				if (!quiet) console.log(`${prefix}Downloaded!`);
-				break;
-		}
-	}
+    function handler(data, prefix) {
+        switch (data.state) {
+            case "authenticated":
+                if (!quiet) console.log(`${prefix}Authenticated, allocating a task...`);
+                break;
+            case "allocated":
+                if (!quiet) console.log(`${prefix}Allocated, submitting the prompt and style...`);
+                break;
+            case "submitted":
+                if (!quiet) console.log(`${prefix}Submitted! Waiting on results...`);
+                break;
+            case "progress":
+                let current = data.task.photo_url_list.length;
+                let max = styles.steps.get(style) + 1;
+                if (!quiet) console.log(`${prefix}Submitted! Waiting on results... (${current}/${max})`);
+                break;
+            case "generated":
+                if (!quiet) console.log(`${prefix}Results are in, downloading the final image...`);
+                break;
+            case "downloaded":
+                if (!quiet) console.log(`${prefix}Downloaded!`);
+                break;
+        }
+    }
 
-	let res = await task(prompt, style, (data) => handler(data, prefix), {final, inter});
-	if (!quiet && final)
-		console.log(`${prefix}Your results have been downloaded to the following files:`);
-	else if (!quiet)
-		console.log(`${prefix}Task finished, the results are available at the following addresses:`);
+    let res = await task(prompt, style, (data) => handler(data, prefix), {final, inter});
+    if (!quiet && final)
+        console.log(`${prefix}Your results have been downloaded to the following files:`);
+    else if (!quiet)
+        console.log(`${prefix}Task finished, the results are available at the following addresses:`);
 
-	for (let inter of res.inter)
-		console.log(inter);
-	
-	if (final) console.log(res.path);
-	else console.log(res.url);
+    for (let inter of res.inter)
+        console.log(inter);
+
+    if (final) console.log(res.path);
+    else console.log(res.url);
 }
 
 (async () => {
-	let prompt = argc._[0];
-	let style = +argc._[1] || 3;
-	if (!quiet)
-		console.log(`Prompt: \`${prompt}\`, Style: \`${styles.get(style)}\``);
+    let prompt = argc._[0];
+    let style = +argc._[1] || 3;
+    if (!quiet)
+        console.log(`Prompt: \`${prompt}\`, Style: \`${styles.get(style)}\``);
 
-	for (let n = 0; n < +argc.times; n++) {
-		const prefix = argc.times == 1 ? `` : `${n+1}: `;
-		if (argc.noasync) await generate(prompt, style, prefix);
-		else generate(prompt, style, prefix);
-	}
+    for (let n = 0; n < +argc.times; n++) {
+        const prefix = argc.times == 1 ? `` : `${n+1}: `;
+        if (argc.noasync) await generate(prompt, style, prefix);
+        else generate(prompt, style, prefix);
+    }
 })();
