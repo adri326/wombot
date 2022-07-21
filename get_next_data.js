2cb41e38773641be7b5c03da35dd2ad141b04231
ErikRospo <rerik.work@gmail.com>
2022-07-21 12:12:04 -0700

    Made further progress on updating the styles.

--- get_next_data.js
+++ get_next_data.js
@@ -19,13 +19,13 @@ function extractContent(element, selector) {
         }
 }
 
-r.then(res=>{
-    let body=extractContent(res,"#__NEXT_DATA__");
-    let data=JSON.parse(body);
-    let styles=data.props.pageProps.artStyles
-    let filtst=filterStyles(styles);
-    fs.writeFileSync(path.join(__dirname,"styles.json"),JSON.stringify(filtst));    
-})
+// r.then(res=>{
+//     let body=extractContent(res,"#__NEXT_DATA__");
+//     let data=JSON.parse(body);
+//     let styles=data.props.pageProps.artStyles
+//     let filtst=filterStyles(styles);
+//     fs.writeFileSync(path.join(__dirname,"styles.json"),JSON.stringify(filtst));    
+// })
 function filterStyles(styles){
     let filtered=[];
     for (let style of styles){
@@ -56,5 +56,20 @@ function updateStyles(){
 function reload(){
     if (checkForUpdate()){
         updateStyles();
+        return true;
     }
+    return false;
 }
+function updatestylesjs(){
+    reload()
+    let styles=JSON.parse(fs.readFileSync(path.join(__dirname,"styles.json")));
+    let s='let styles = new Map();\n';
+    for (let style of styles.styles){
+        s+='styles.set('+style.id+',"'+style.name+'");\n';
+    }
+    s+='let steps = new Map();steps.set(1, 23);\nsteps.set(2, 21);\nsteps.set(3, 23);\nsteps.set(4, 23);\nsteps.set(5, 19);\nsteps.set(6, 20);\nsteps.set(7, 21);\nsteps.set(8, 21);\nsteps.set(9, 21);\nsteps.set(10, 20);\nsteps.set(11, 20);\nsteps.set(12, 32);\nsteps.set(13, 20);\nsteps.set(14, 20);\nsteps.set(15, 20);\nsteps.set(16, 20);\nsteps.set(17, 19);\nsteps.set(18, 19);\nsteps.set(19, 20);\nsteps.set(20, 20);\nsteps.set(21, 20);\nsteps.set(22, 20);\nsteps.set(23, 20);\nsteps.set(24, 19);\nsteps.set(25, 19);\nsteps.set(26, 19);\nsteps.set(27, 19);\nsteps.set(28, 19);'
+    s+='module.exports = styles;';
+    s+='module.exports.steps = steps;';
+    fs.writeFileSync(path.join(__dirname,"styles.js"),s);
+}
+updatestylesjs();
--- styles.js
+++ styles.js
@@ -1,38 +1,28 @@
 let styles = new Map();
-styles.set(1, "Synthwave");
-styles.set(2, "Ukiyoe");
-styles.set(3, "No Style");
-styles.set(4, "Steampunk");
-styles.set(5, "Fantasy Art");
-styles.set(6, "Vibrant");
-styles.set(7, "HD");
-styles.set(8, "Pastel");
-styles.set(9, "Psychic");
-styles.set(10, "Dark Fantasy");
-styles.set(11, "Mystical");
-styles.set(12, "Festive");
-styles.set(13, "Baroque");
-styles.set(14, "Etching");
-styles.set(15, "S. Dali"); // Note: The website didn't put a space in it
-styles.set(16, "Wuhtercuhler");
-styles.set(17, "Provenance");
-styles.set(18, "Rose Gold");
-styles.set(19, "Moonwalker");
-styles.set(20, "Blacklight");
-styles.set(21, "Psychedelic");
-styles.set(22, "Ghibli");
-styles.set(23, "Surreal");
-// these styles seem to have been removed and aren't available anymore
-// styles.set(24, "Love");
-// styles.set(25, "Death");
-// styles.set(26, "Robots");
-styles.set(27, "Radioactive");
-styles.set(28, "Arcane");
-
-// Counts the number of GANs in the chain, used for debugging
-// Does not include the "final" image!
-let steps = new Map();
-steps.set(1, 23);
+styles.set(40,"Malevolent");
+styles.set(3,"No Style");
+styles.set(22,"Ghibli");
+styles.set(28,"Melancholic");
+styles.set(17,"Provenance");
+styles.set(34,"Arcane");
+styles.set(27,"Radioactive");
+styles.set(20,"Blacklight");
+styles.set(18,"Rose Gold");
+styles.set(16,"Wuhtercuhler");
+styles.set(15,"S.Dali");
+styles.set(14,"Etching");
+styles.set(13,"Baroque");
+styles.set(11,"Mystical");
+styles.set(10,"Dark Fantasy");
+styles.set(9,"Psychic");
+styles.set(7,"HD");
+styles.set(6,"Vibrant");
+styles.set(5,"Fantasy Art");
+styles.set(4,"Steampunk");
+styles.set(21,"Psychedelic");
+styles.set(2,"Ukiyoe");
+styles.set(1,"Synthwave");
+let steps = new Map();steps.set(1, 23);
 steps.set(2, 21);
 steps.set(3, 23);
 steps.set(4, 23);
@@ -48,8 +38,8 @@ steps.set(13, 20);
 steps.set(14, 20);
 steps.set(15, 20);
 steps.set(16, 20);
-steps.set(17, 19); // it seems like image 12 gets skipped
-steps.set(18, 19); // same deal here
+steps.set(17, 19);
+steps.set(18, 19);
 steps.set(19, 20);
 steps.set(20, 20);
 steps.set(21, 20);
@@ -59,7 +49,4 @@ steps.set(24, 19);
 steps.set(25, 19);
 steps.set(26, 19);
 steps.set(27, 19);
-steps.set(28, 19);
-
-module.exports = styles;
-module.exports.steps = steps;
+steps.set(28, 19);module.exports = styles;module.exports.steps = steps;
