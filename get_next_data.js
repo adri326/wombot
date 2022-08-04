8b8fff472d658c2b7ed731ffa075fa1800ae8f78
ErikRospo <87732519+ErikRospo@users.noreply.github.com>
2022-08-04 14:23:28 -0700

    changed to a better source of nextdata

--- get_next_data.js
+++ get_next_data.js
@@ -1,65 +1,14 @@
+//@ts-check
 const rest=require("./rest.js");
 const fs=require("fs");
 const path=require("path");
 
-const wombo_rest=new rest("app.wombo.art",100);
+const style_rest=new rest("https://app.wombo.art",100);
 
-const r=wombo_rest.get("")
-function extractnextdata(element){
-    const regex = /(?:"__NEXT_DATA__"[\w\W]*)">([\w\W]*)(?:<\/script>)/gm;
-    let match;
-    while (match = regex.exec(element)) {
-        return match[1];
-    }
+function getNEXTdata(){
+    let a=await style_rest.get("/api/styles")
 }
-function filterStyles(styles){
-    let filtered=[];
-    for (let style of styles){
-        filtered.push({'id':style.id,'name':style.name,'photo':style.photo_url})
-    }
-    filtered.sort((a,b)=>{b.id-a.id});
-    filtered={'styles':filtered,'updated':new Date().getDay()};
-    return filtered;
-}
-function checkForUpdate(){
-    if (!fs.existsSync(path.join(__dirname,"styles.json"))){
-        fs.openSync(path.join(__dirname,"styles.json"),"w");
-    }
-    try{
-        let styles=JSON.parse(fs.readFileSync(path.join(__dirname,"styles.json")));
-        let lastUpdate=styles.updated;
-        let today=new Date().getDay();
-        return today!=lastUpdate;
-    }
-    catch(e){
-        return true;
-    }
-    
-}
-function updateStyles(){
-    
-    const wombo_rest=new rest("app.wombo.art",100);
 
-    const r=wombo_rest.get("");
-    r.then(res=>{
-        let nextdata=extractnextdata(res);
-        let data=JSON.parse(nextdata);
-        let styles=data.props.pageProps.artStyles
-        let filtst=filterStyles(styles);
-        if (!fs.existsSync(path.join(__dirname,"styles.json"))){
-            fs.openSync(path.join(__dirname,"styles.json"),"w");
-            
-        }
-        fs.writeFileSync(path.join(__dirname,"styles.json"),JSON.stringify(filtst));  
-          
-    })}
-function reload(){
-    if (checkForUpdate()){
-        updateStyles();
-        return true;
-    }
-    return false;
-}
 function updatestylesjs(){
     reload()
     
